051a08de369548d66838a3d310588f7c8949f1c9
Brendon Bitencourt <brendon.braga@gmail.com>
2019-05-25 19:58:45 -0300

    Angular Material Design

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModules } from '../shared/material-modules.const';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ application/src/shared/material-modules.const.ts
@@ -0,0 +1,15 @@
+import { MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+/**
+ * Exporta todos os modulos do angular material design
+ * @url https://material.angular.io/
+ */
+export const MaterialModules = [
+  MatInputModule,
+  MatFormFieldModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+];
